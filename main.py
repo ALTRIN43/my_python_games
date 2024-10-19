17ab21501250a9bd0c320ae4f16ce19321d0a902
ALTRIN RAJ <josephaltrin7@gmail.com>
2024-10-19 12:36:06 +0530

    Add files via upload
    Snake Game

--- main.py
+++ main.py
@@ -1,53 +1,68 @@
-from turtle import Turtle, Screen
-from paddle import Paddle
-from ball import Ball
+"""
+The first step is to breakdown the problem to several parts
+part 1 : create a snake body
+part 2 : move the snake
+part 3 : control the snake
+part 4 : detect the collision with food
+part 5 : create a scoreboard
+part 6 : detect collision with wall
+part 7 : detect collision with tail
+"""
+
+
+
+from turtle import Screen
+
+from matplotlib.pyplot import xcorr
+
+from snake import Snake
+from food import Food
 from score import Score
 import time
 
 screen = Screen()
-screen.setup(800, 600)
-screen.bgcolor('black')
-screen.title("PONG GAME")
+screen.setup(600, 600)
+screen.bgcolor("black")
+screen.title("Snake Game")
 screen.tracer(0)
 
-r_paddle = Paddle((350, 0))
-l_paddle = Paddle((-359, 0))
-ball = Ball((0,0))
+snake = Snake()
+food = Food()
 score = Score()
 
 screen.listen()
-screen.onkeypress(r_paddle.go_up, "Up")
-screen.onkeypress(r_paddle.go_down, "Down")
-screen.onkeypress(l_paddle.go_up, "w")
-screen.onkeypress(l_paddle.go_down, "s")
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 game_is_on = True
+
 while game_is_on:
-    time.sleep(ball.move_speed)
     screen.update()
-    ball.move()
+    time.sleep(0.1)
 
-    #detect the collision with wall
-    if ball.ycor() > 280 or ball.ycor() < -280:
-        # bounce
-        ball.bounce_y()
+    snake.move()
 
-    #detect the collision with paddles
-    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
-        ball.bounce_x()
+    #Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        score.increase_score()
+        snake.extend()
 
-    #detect r paddle miss
-    if ball.xcor() > 380:
-        ball.reset_position()
-        score.l_point()
+    #Detect collision with wall
+    if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
+        game_is_on = False
+        score.game_over()
 
-    #detect l paddle miss
-    if ball.xcor() < -380:
-        ball.reset_position()
-        score.r_point()
-
-    screen.update()
-
+    #Detect collision with tail
+    for segment in snake.segments[1:]:
+        # if segment == snake.head:
+        #     pass
+        #el
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            score.game_over()
 
 
 
--- score.py
+++ score.py
@@ -1,27 +1,27 @@
 from turtle import Turtle
+ALIGNMENT = "CENTER"
+FONT = ("courier", 24, "normal")
 
 class Score(Turtle):
 
     def __init__(self):
         super().__init__()
+        self.score = 0
         self.color("white")
         self.penup()
+        self.goto(0, 260)
         self.hideturtle()
-        self.l_score = 0
-        self.r_score = 0
         self.update_score()
 
     def update_score(self):
+        self.write(f"Score:{self.score}", align=ALIGNMENT, font=FONT)
+
+    def game_over(self):
+        self.goto(0,0)
+        self.write("GAME OVER", align=ALIGNMENT, font= FONT)
+
+
+    def increase_score(self):
+        self.score += 1
         self.clear()
-        self.goto(-100, 200)
-        self.write(self.l_score, align='center', font=('courier', 50, 'normal'))
-        self.goto(100, 200)
-        self.write(self.r_score, align='center', font=('courier', 50, 'normal'))
-
-    def l_point(self):
-        self.l_score += 1
-        self.update_score()
-
-    def r_point(self):
-        self.r_score += 1
         self.update_score()
