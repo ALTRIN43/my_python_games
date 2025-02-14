2fc42cb0192ca415a6bb1dc5c692af9339aa6000
ALTRIN JESURAJ <josephaltrin7@gmail.com>
2025-02-14 22:42:35 +0530

    Update ball.py

--- ball.py
+++ ball.py
@@ -7,7 +7,7 @@ class Ball(Turtle):
     def __init__(self, position):
         super().__init__()
         self.shape("circle")
-        self.color("white")
+        self.color("black")
         self.goto(position)
         self.penup()
         self.x_move = 10
